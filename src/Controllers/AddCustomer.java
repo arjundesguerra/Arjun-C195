029b3637bf6e5e6844599429a89e43412ec5868f
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-16 05:14:59 -0700

    added textfields to add customer and added cancel button

--- src/Controllers/AddCustomer.java
+++ src/Controllers/AddCustomer.java
@@ -1,8 +1,28 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class AddCustomer {
+
+    @FXML
+    private Button cancelButton;
+
+    public void goToHomepage() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXMLViews/Homepage.fxml"));
+        Scene scene = new Scene(root);
+        Stage newStage = new Stage();
+        newStage.setTitle("Homepage");
+        newStage.setScene(scene);
+        newStage.show();
+        Stage currentStage = (Stage) cancelButton.getScene().getWindow();
+        currentStage.close();
+    }
 
 }
