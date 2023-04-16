624aee1584830cc6154888f4f6af7a29a29c62c3
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-16 04:12:24 -0700

    changed password to passwordfield and added tabPane

--- src/Controllers/LoginForm.java
+++ src/Controllers/LoginForm.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -17,7 +18,7 @@ import java.time.ZoneId;
 public class LoginForm {
 
     @FXML private TextField usernameField;
-    @FXML private TextField passwordField;
+    @FXML private PasswordField passwordField;
     @FXML private Button signInButton;
     @FXML private Text locationText;
 
