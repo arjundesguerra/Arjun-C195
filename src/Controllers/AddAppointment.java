0151204e6fce78f572dcf52853f2ad44038a8b53
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-23 01:45:01 -0700

    customer combo box populated with customer names

--- src/Controllers/AddAppointment.java
+++ src/Controllers/AddAppointment.java
@@ -1,18 +1,33 @@
 package Controllers;
 
+import Database.CustomerHelper;
+import Models.Customer;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class AddAppointment {
 
     @FXML private TextField titleTextField;
+    @FXML private ComboBox customerComboBox;
+    @FXML private Button submitButton;
+
+    public void initialize() throws SQLException {
+        submitButton.setFocusTraversable(true);
+        Platform.runLater(() -> submitButton.requestFocus());
+
+        setCustomerComboBox();
+    }
 
     public void goToAppointmentHomepage() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/FXMLViews/AppointmentHomepage.fxml"));
@@ -23,5 +38,13 @@ public class AddAppointment {
         newStage.show();
         Stage currentStage = (Stage) titleTextField.getScene().getWindow();
         currentStage.close();
+    }
+
+    public void setCustomerComboBox() throws SQLException {
+        ObservableList<Customer> customerList = CustomerHelper.fetchCustomers();
+        for (Customer customer : customerList) {
+            customerComboBox.getItems().add(customer.getCustomerName());
+        }
+
     }
 }
--- src/Database/AppointmentHelper.java
+++ src/Database/AppointmentHelper.java
@@ -36,4 +36,5 @@ public class AppointmentHelper {
 
         return appointmentList;
     }
+
 }
